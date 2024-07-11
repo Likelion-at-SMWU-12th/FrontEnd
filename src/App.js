04a900c9a6283b090495c675893497c42e50b88c
solux <lee.yeji0327@gmail.com>
2024-07-11 16:27:20 +0900

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import "./App.css";
+import styled from "styled-components";
+import CommentPage from "./pages/CommentPage";
+import WritePage from "./pages/WritePage";
+import DetailPage from "./pages/DetailPage";
+
+const Wrapper = styled.div`
+  padding: 80px 100px;
+  width: calc(100vw - 200px);
+  background-color: #f0f0f0;
+  height: 100vh;
+`;
+const Title = styled.div`
+  font-size: 50px;
+  font-weight: bolder;
+  cursor: pointer;
+`;
 
 function App() {
+  const navigate = useNavigate();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Title onClick={() => navigate("/")}>숙멋 프론트의 방명록 🦁</Title>
+      <Routes>
+        <Route path="/" element={<CommentPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/comment/:id" element={<DetailPage />} />
+      </Routes>
+    </Wrapper>
   );
 }
 
---
+++ src/components/Comment.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const CommentBox = styled.div`
+  width: 100%;
+  height: 100px;
+  margin-bottom: 50px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.15);
+  border-radius: 20px;
+  padding: 50px;
+  background-color: white;
+  &:hover {
+    cursor: pointer;
+    color: #f0873e;
+    box-shadow: 0 0 10px rgba(238, 110, 67, 0.727);
+    background-color: #fef5f0;
+    > span {
+      color: #ffcca8;
+    }
+  }
+`;
+const Author = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+`;
+const Time = styled.span`
+  font-size: 15px;
+  color: #c8c8c8;
+  font-weight: 600;
+  margin-top: 5px;
+`;
+const CommentTxt = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 20px;
+`;
+
+let dummy = [
+  {
+    id: "1",
+    author: "이예지",
+    comment: "하이",
+    timestamp: "2024-06-28T12:34:56Z",
+  },
+  {
+    id: "2",
+    author: "박상아",
+    comment: "상아언니의 세미나",
+    timestamp: "2024-06-28T12:34:56Z",
+  },
+  {
+    id: "3",
+    author: "이선아",
+    comment: "상아언니의 세미나",
+    timestamp: "2024-06-28T12:34:56Z",
+  },
+  {
+    id: "4",
+    author: "조성하",
+    comment: "상아언니의 세미나",
+    timestamp: "2024-06-28T12:34:56Z",
+  },
+  {
+    id: "5",
+    author: "강주은",
+    comment: "상아언니의 세미나",
+    timestamp: "2024-06-28T12:34:56Z",
+  },
+];
+
+const Comment = () => {
+  const navigate = useNavigate();
+  return (
+    <>
+      {dummy.map((comment) => (
+        <CommentBox onClick={() => navigate(`/comment/${comment.id}`)}>
+          <Author>{comment.author}</Author>
+          <Time>{comment.timestamp}</Time>
+          <CommentTxt>{comment.comment}</CommentTxt>
+        </CommentBox>
+      ))}
+    </>
+  );
+};
+
+export default Comment;
